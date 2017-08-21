de2b1585c18061f430a6dba3109e67124c17c201
prozhou <zhoushengzheng@gmail.com>
2017-08-21 19:32:00 +0800

    bug fix

--- hole.sh
+++ hole.sh
@@ -7,8 +7,7 @@ function monitor_service(){
         return 0
     fi
 
-
-    status_code=`curl -I -m 10 -o /dev/null -s -w %{http_code} $domain`
+    status_code=`curl -I -m 15 -o /dev/null -s -w %{http_code} $domain`
     if (($status_code == 200 )); then
         return 0
     else
@@ -18,38 +17,39 @@ function monitor_service(){
 }
 
 
-
 function ssh_con(){
-   loc_nu=$(echo $1 | grep "localhost"|wc -l)
-   if [ $loc_nu -eq 0 ]; then
-     return
-   fi
+   # analyse the service conn
+   loc_nu=$(ps -ef | grep -v grep | grep "$(echo $1)" | wc -l)
 
    ssh_str="ssh -CfNgqP -R $1"
    remote_str=$(echo $1 | awk '{print $2}')
-   local_str=$(echo $1 | awk '{print $1}')
-
    ssh_port=$(echo $1 | awk '{print $1}'| awk -F ":" '{print $3}')
-   echo $local_str
-   SSH_NUM=$(ps -ef |grep -v grep | grep $local_str |wc -l)
-   echo $SSH_NUM
 
-   if [ $SSH_NUM -gt 0 ] ;then
-        #restart remote service
-       monitor_service  $2
-       if [ $? -gt 0 ] || [ $SSH_NUM -gt 1 ] ;then
-         echo 'reset service'
-         ps -ef | grep -v grep|grep "$local_str" | awk '{print $2}' | xargs kill -9
-         ssh $remote_str "ps -ef |grep -v grep | grep -v pts| grep 'sshd: root'| awk '{print \$2 }'| xargs kill -9 "
-       else
-         echo "service $ssh_port is ok"
-       fi
-
-   else
-       echo "$ssh_port is reconnecting"
-       $ssh_str
-
+   # local conn has one, and service is response
+   if [ $loc_nu -eq 1 ]; then
+     echo "$ssh_port local conn is ok"
+     monitor_service  $2
+     if [ $? -gt 0 ]  ;then
+        echo "$ssh_port  is not servicing"
+        echo "$ssh_port  remote and local is killed"
+        ps -ef | grep -v grep| grep "$(echo $1)" | awk '{print $2}' | xargs kill -9
+        ssh $remote_str "netstat -anp | grep 0.0.0.0:$ssh_port | awk '{print \$7}' | awk -F "/" '{print \$1}'| xargs kill -9 "
+     else
+        echo "$ssh_port remote service is ok"
+     fi
+     return
    fi
+
+
+
+   if [ $loc_nu -gt 1 ] ;then
+     #reset local conn
+     echo "$ssh_port local is reset"
+     ps -ef | grep -v grep| grep "$(echo $1)" | awk '{print $2}' | xargs kill -9
+   fi
+
+   echo "$ssh_port is connecting"
+   $ssh_str
 }
 
 
@@ -65,7 +65,6 @@ do
     else
         ssh_con "$ssh_port"
     fi
-
 done
 
 
